58bc802dd2c00df1e1fb8186c87e0ab5256f8e87
leaderman <miao.work2010@gmail.com>
2025-04-27 16:49:55 +0800

    添加函数：执行 GET 请求

---
+++ http.js
@@ -0,0 +1,68 @@
+window.tm = window.tm || {};
+window.tm.http = window.tm.http || {};
+
+window.tm.http._getHeader = function (headers, name) {
+  if (!headers) {
+    return null;
+  }
+
+  return (
+    headers
+      ?.split(/\r?\n/)
+      .find((header) =>
+        header.toLowerCase().startsWith(`${name.toLowerCase()}:`)
+      )
+      ?.split(":")[1]
+      ?.trim() || null
+  );
+};
+
+window.tm.http.get = function (url, headers = {}) {
+  return new Promise((resolve, reject) => {
+    GM_xmlhttpRequest({
+      method: "GET",
+      url,
+      headers,
+      responseType: "json",
+      onload: function (response) {
+        try {
+          const requestId = window.tm.http._getHeader(
+            response.responseHeaders,
+            "x-request-id"
+          );
+
+          if (response.status !== 200) {
+            return reject(
+              new Error(`请求 ID: ${requestId} 状态码异常: ${response.status}`)
+            );
+          }
+
+          const result = response.response;
+
+          if (result && typeof result.code === "number") {
+            if (result.code === 200) {
+              resolve(result.data);
+            } else {
+              reject(
+                new Error(
+                  `请求 ID: ${requestId} 响应错误, 错误码: ${result.code}, 错误信息: ${result.message}`
+                )
+              );
+            }
+          } else {
+            reject(
+              new Error(
+                `请求 ID: ${requestId} 响应错误, 格式异常, 内容: ${response.response}`
+              )
+            );
+          }
+        } catch (e) {
+          reject(e);
+        }
+      },
+      onerror: function (error) {
+        reject(error);
+      },
+    });
+  });
+};
