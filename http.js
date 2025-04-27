99058fc3d9625a81eaa1dd3ea589a33b6e1e2e77
leaderman <miao.work2010@gmail.com>
2025-04-27 16:59:07 +0800

    添加函数：执行 POST 请求

--- http.js
+++ http.js
@@ -17,11 +17,75 @@ window.tm.http._getHeader = function (headers, name) {
   );
 };
 
+/**
+ * 发送 GET 请求
+ * @param {string} url  请求的 URL
+ * @param {Object} headers  请求头
+ * @returns {Promise<any>} 请求结果
+ */
 window.tm.http.get = function (url, headers = {}) {
   return new Promise((resolve, reject) => {
     GM_xmlhttpRequest({
       method: "GET",
       url,
+      headers,
+      responseType: "json",
+      onload: function (response) {
+        try {
+          const requestId = window.tm.http._getHeader(
+            response.responseHeaders,
+            "x-request-id"
+          );
+
+          if (response.status !== 200) {
+            return reject(
+              new Error(`请求 ID: ${requestId} 状态码异常: ${response.status}`)
+            );
+          }
+
+          const result = response.response;
+
+          if (result && typeof result.code === "number") {
+            if (result.code === 200) {
+              resolve(result.data);
+            } else {
+              reject(
+                new Error(
+                  `请求 ID: ${requestId} 响应错误, 错误码: ${result.code}, 错误信息: ${result.message}`
+                )
+              );
+            }
+          } else {
+            reject(
+              new Error(
+                `请求 ID: ${requestId} 响应错误, 格式异常, 内容: ${response.response}`
+              )
+            );
+          }
+        } catch (e) {
+          reject(e);
+        }
+      },
+      onerror: function (error) {
+        reject(error);
+      },
+    });
+  });
+};
+
+/**
+ * 发送 POST 请求
+ * @param {string} url  请求的 URL
+ * @param {Object} data  请求体
+ * @param {Object} headers  请求头
+ * @returns {Promise<any>} 请求结果
+ */
+window.tm.http.post = function (url, data, headers = {}) {
+  return new Promise((resolve, reject) => {
+    GM_xmlhttpRequest({
+      method: "POST",
+      url,
+      data,
       headers,
       responseType: "json",
       onload: function (response) {
